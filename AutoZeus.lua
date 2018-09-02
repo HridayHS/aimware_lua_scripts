c8f01131e0f71eb3f8b89be0ec7ad6ee91b28b2b
Hriday Sharma <hridaysharma42@gmail.com>
2018-09-02 17:04:19 +0530

    AutoZeus: Clean up and Improve code quality

--- AutoZeus.lua
+++ AutoZeus.lua
@@ -1,29 +1,30 @@
 local SET = gui.SetValue;
-local userid_to_index = client.GetPlayerIndexByUserID;
-local get_local_player = client.GetLocalPlayerIndex;
 
-local function on_item_equip(Event)
-	-- returns if the current event is not the item_equip event
+function AutoZeus( Event )
 	if (Event:GetName() ~= 'item_equip') then
 		return;
 	end
 
-	local local_player, userid, item, weptype = get_local_player(), Event:GetInt('userid'), Event:GetString('item'), Event:GetInt('weptype');
+	local ME = client.GetLocalPlayerIndex();
+	local PlayerIndex = client.GetPlayerIndexByUserID;
+	local INT_UID = Event:GetInt( 'userid' );
+	local item = Event:GetString('item');
 
-	-- checks if the local players index is equal to the player that called the event
-	if (local_player == userid_to_index(userid)) then
+	if ( ME == PlayerIndex( INT_UID ) ) then
+
 		if (item == "taser") then
-			SET("rbot_active", 1); -- Master Switch: On
+			SET("rbot_active", 1); -- Ragebot Master Switch: On
 			SET("rbot_enable", 1);
 			SET("rbot_fov", 30);
 			SET("rbot_silentaim", 1); -- Silent Aim: On
 			SET("rbot_rifle_hitchance", 80); -- Zeus hitchance
-			SET("rbot_speedlimit", 1);
+			SET("rbot_speedlimit", 1); -- Speed limit: On
 		else
-			SET("rbot_active", 0); -- Master Switch: Off
+			SET("rbot_active", 0); -- Ragebot Master Switch: Off
 		end
 	end
 end
 
-client.AllowListener('item_equip');
-callbacks.Register("FireGameEvent", "on_item_equip", on_item_equip);
+client.AllowListener( 'item_equip' );
+
+callbacks.Register( "FireGameEvent", "AutoZeus", AutoZeus );
