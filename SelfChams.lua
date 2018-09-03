1699754db6be46d6f7127d11b822e47f56817463
Hriday Sharma <hridaysharma42@gmail.com>
2018-09-03 06:03:17 +0530

    Add Self Chams lua
    This lua disables Chams when scoped in.

---
+++ SelfChams.lua
@@ -0,0 +1,19 @@
+local GET = gui.GetValue;
+local SET = gui.SetValue;
+
+local UserChamsValue = GET("esp_self_chams");
+
+local function SelfChams()
+
+	local LocalPlayer = entities.GetLocalPlayer();
+	local isScoped = LocalPlayer:GetProp( 'm_bIsScoped' );
+	
+	if isScoped == 1 then
+		SET("esp_self_chams", 0);
+	else
+		SET("esp_self_chams", UserChamsValue);
+	end
+	
+end
+
+callbacks.Register( "Draw", "SelfChams", SelfChams);
