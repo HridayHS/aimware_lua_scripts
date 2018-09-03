4f7254fb9ab5c14ccdc89a74618a84834a1c5a5e
Hriday Sharma <hridaysharma42@gmail.com>
2018-09-03 05:55:30 +0530

    ZoomFOV: Update!
    Fix everything. Use IsScoped entity function instead of weapon_zoom.

--- ZoomFOV.lua
+++ ZoomFOV.lua
@@ -1,30 +1,16 @@
-local GET = gui.GetValue;
 local SET = gui.SetValue;
 
-local ME = client.GetLocalPlayerIndex();
-local PlayerIndex = client.GetPlayerIndexByUserID;
-
-local function ZoomFOV( Event )
-
-	local INT_UID = Event:GetInt( 'userid' );
+local function ZoomFOV()
 	
-	if ( Event:GetName() == 'weapon_zoom' or Event:GetName() == 'weapon_zoom_rifle' ) then
+	local LocalPlayer = entities.GetLocalPlayer();
+	local isScoped = LocalPlayer:GetProp( 'm_bIsScoped' );
 	
-		if ( ME == PlayerIndex( INT_UID ) ) then
-			SET("vis_view_fov", 0);
-		end
-
-	elseif ( Event:GetName() ~= 'weapon_zoom' or Event:GetName() ~= 'weapon_zoom_rifle' ) then
-
-		if ( ME == PlayerIndex( INT_UID ) ) then
-			SET("vis_view_fov", 110);
-		end
-
+	if isScoped == 1 then
+		SET("vis_view_fov", 0);
+	else
+		SET("vis_view_fov", 110);
 	end
 	
 end
 
-client.AllowListener( 'weapon_zoom' );
-client.AllowListener( 'weapon_zoom_rifle' );
-
-callbacks.Register( "FireGameEvent", "ZoomFOV", ZoomFOV);
+callbacks.Register( "Draw", "ZoomFOV", ZoomFOV);
