81827c744150f54bc5ab435b422bd2680b0ddbcf
Hriday Sharma <hridaysharma42@gmail.com>
2018-09-20 13:19:35 +0530

    AutoZeus: Only run if Legit Bot is enabled

--- AutoZeus.lua
+++ AutoZeus.lua
@@ -1,27 +1,33 @@
 local SET = gui.SetValue;
 
 function AutoZeus( Event )
-	if (Event:GetName() ~= 'item_equip') then
-		return;
-	end
 
-	local ME = client.GetLocalPlayerIndex();
-	local PlayerIndex = client.GetPlayerIndexByUserID;
-	local INT_UID = Event:GetInt( 'userid' );
-	local item = Event:GetString('item');
+	if ( gui.GetValue("lbot_active") == true ) then
 
-	if ( ME == PlayerIndex( INT_UID ) ) then
-
-		if (item == "taser") then
-			SET("rbot_active", 1); -- Ragebot Master Switch: On
-			SET("rbot_enable", 1);
-			SET("rbot_fov", 30);
-			SET("rbot_silentaim", 1); -- Silent Aim: On
-			SET("rbot_rifle_hitchance", 80); -- Zeus hitchance
-			SET("rbot_speedlimit", 1); -- Speed limit: On
-		else
-			SET("rbot_active", 0); -- Ragebot Master Switch: Off
+		if (Event:GetName() ~= 'item_equip') then
+			return;
 		end
+
+		local ME = client.GetLocalPlayerIndex();
+		local PlayerIndex = client.GetPlayerIndexByUserID;
+		local INT_UID = Event:GetInt( 'userid' );
+		local item = Event:GetString( 'item' );
+
+		if ( ME == PlayerIndex( INT_UID ) ) then
+
+			if (item == "taser") then
+				SET("rbot_active", 1); -- Ragebot Master Switch: On
+				SET("rbot_enable", 1);
+				SET("rbot_fov", 15);
+				SET("rbot_silentaim", 2); -- Silent Aim: On
+				SET("rbot_rifle_hitchance", 80); -- Zeus hitchance
+				SET("rbot_speedlimit", 1); -- Speed limit: On
+			else
+				SET("rbot_active", 0); -- Ragebot Master Switch: Off
+			end
+			
+		end
+		
 	end
 end
 
