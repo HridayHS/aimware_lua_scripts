20d965dac20529506e41439af1edfcc212c0504e
Hriday Sharma <hridaysharma42@gmail.com>
2018-10-07 13:56:19 +0530

    AutoZeus: Update!
    - Add GUI Checkbox. ( LEGIT > Aimbot )
    - Improve code
    - Disable Silent Aim
    - Turn on Position Adjustment if Legitbot Position Adjustment is also turned on.

--- AutoZeus.lua
+++ AutoZeus.lua
@@ -1,36 +1,50 @@
-local SET = gui.SetValue;
+local SetValue = gui.SetValue;
 
-function AutoZeus( Event )
+local LEGIT_AIMBOT_REF = gui.Reference( "LEGIT", "Aimbot" );
 
-	if ( gui.GetValue("lbot_active") == true ) then
+local AWAutoZeus = gui.Checkbox( LEGIT_AIMBOT_REF, "lua_lbot_autozeus", "Auto Zeus", 0 );
 
-		if (Event:GetName() ~= 'item_equip') then
-			return;
-		end
+local function AutoZeus( Event )
 
-		local ME = client.GetLocalPlayerIndex();
-		local PlayerIndex = client.GetPlayerIndexByUserID;
-		local INT_UID = Event:GetInt( 'userid' );
-		local item = Event:GetString( 'item' );
-
-		if ( ME == PlayerIndex( INT_UID ) ) then
-
-			if (item == "taser") then
-				SET("rbot_active", 1); -- Ragebot Master Switch: On
-				SET("rbot_enable", 1);
-				SET("rbot_fov", 15);
-				SET("rbot_silentaim", 2); -- Silent Aim: On
-				SET("rbot_rifle_hitchance", 80); -- Zeus hitchance
-				SET("rbot_speedlimit", 1); -- Speed limit: On
-			else
-				SET("rbot_active", 0); -- Ragebot Master Switch: Off
-			end
-			
-		end
+	if gui.GetValue("lbot_active") == true then
 		
+		if AWAutoZeus:GetValue() then
+		
+			if ( Event:GetName() ~= "item_equip" ) then
+				return;
+			end
+
+			local ME = client.GetLocalPlayerIndex();
+			local INT_UID = Event:GetInt( "userid" );
+			local PlayerIndex = client.GetPlayerIndexByUserID( INT_UID );
+
+			local WepType = Event:GetInt( "weptype" );
+			local Item = Event:GetString( "item" );
+
+			if ( ME == PlayerIndex ) then
+				if ( Item == "taser" ) then
+					SetValue("rbot_active", 1);
+					SetValue("rbot_enable", 1);
+					SetValue("rbot_fov", 15);
+					SetValue("rbot_speedlimit", 1);
+					SetValue("rbot_silentaim", 0);
+					if ( gui.GetValue( "lbot_positionadjustment" ) > 0 ) then
+						SetValue("rbot_positionadjustment", 5)
+					else
+						SetValue("rbot_positionadjustment", 0)
+					end
+				else
+					SetValue("rbot_active", 0);
+				end
+
+			end
+
+		end
+
 	end
+
 end
 
-client.AllowListener( 'item_equip' );
+client.AllowListener( "item_equip" )
 
-callbacks.Register( "FireGameEvent", "AutoZeus", AutoZeus );
+callbacks.Register( "FireGameEvent", "AutoZeus", AutoZeus )
