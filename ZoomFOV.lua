35d29fe29522c7432da56cee62766cabbe46ecf6
Hriday Sharma <hridaysharma42@gmail.com>
2018-10-09 01:10:18 +0530

    ZoomFOV: Update!
    - Fix when shift-walking
    - Menu integration ( VISUALS > Main )

--- ZoomFOV.lua
+++ ZoomFOV.lua
@@ -1,25 +1,32 @@
-local GET = gui.GetValue;
-local SET = gui.SetValue;
+local GetValue = gui.GetValue;
+local SetValue = gui.SetValue;
 
-local UserViewFov = GET("vis_view_fov");
+local VIS_MAIN_REF = gui.Reference( "VISUALS", "Main" );
+local ForceDoubleScope = gui.Checkbox( VIS_MAIN_REF, "lua_forcedoublescope", "Force Double Scope", 0 );
+
+local UserViewFov = GetValue( "vis_view_fov" );
 
 local function ZoomFOV()
 	
-	if ( UserViewFov > 0 ) then
+	if ForceDoubleScope:GetValue() then
 	
-		if entities.GetLocalPlayer() ~= nil then
+		if ( UserViewFov > 0 ) then
 
-			local LocalPlayer = entities.GetLocalPlayer();
-			local isScoped = LocalPlayer:GetProp( 'm_bIsScoped' );
-	
-			if isScoped == 1 then
-				SET("vis_view_fov", 0);
-			else
-				SET("vis_view_fov", UserViewFov);
+			if entities.GetLocalPlayer() ~= nil then
+
+				local LocalPlayer = entities.GetLocalPlayer();
+				local isScoped = LocalPlayer:GetProp( "m_bIsScoped" );
+				
+				if isScoped == 1 or isScoped == 257 then
+					SetValue( "vis_view_fov", 0 );
+				else
+					SetValue( "vis_view_fov", UserViewFov );
+				end
+
 			end
 
 		end
-
+	
 	end
 	
 end
