318ab355487adb8f3e89cdf29b51f8d45ebfd179
HridayHS <hridaysharma42@gmail.com>
2018-10-16 16:32:36 +0530

    SniperCrosshair: Fix while moving and only show when alive

--- SniperCrosshair.lua
+++ SniperCrosshair.lua
@@ -7,23 +7,27 @@ local function SniperCrosshair()
 	if AWSniperCrosshair:GetValue() then
 		if entities.GetLocalPlayer() ~= nil then
 			local LocalPlayerEntity = entities.GetLocalPlayer();
+			local Alive = LocalPlayerEntity:IsAlive();
+			
 			local isScoped = LocalPlayerEntity:GetPropInt( "m_bIsScoped" );
 	
 			local Thirdperson = gui.GetValue( "vis_thirdperson_dist" );
 			local Scoperemover = gui.GetValue( "vis_scoperemover" );
 
+			if Alive then
 			if Scoperemover ~= 1 then
-				if ( isScoped == 1 or Thirdperson > 0 ) then
+				if ( ( isScoped == 1 or isScoped == 257 ) or Thirdperson > 0 ) then
 					client.SetConVar( "weapon_debug_spread_show", 0, true );
 				else
 					client.SetConVar( "weapon_debug_spread_show", 3, true );
 				end
 			elseif Scoperemover == 1 then
-				if ( Thirdperson > 0 and isScoped == 0 ) then
+				if ( Thirdperson > 0 and ( isScoped == 0 or isScoped == 257 ) ) then
 					client.SetConVar( "weapon_debug_spread_show", 0, true );
 				else
 					client.SetConVar( "weapon_debug_spread_show", 3, true );
 				end
+			end
 			end
 		end
 	else
