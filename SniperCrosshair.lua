c289f7ef587ed62ff1e78bcd5a657d6d59a111e1
HridayHS <hridaysharma42@gmail.com>
2018-10-18 18:47:43 +0530

    SniperCrosshair: Fix pHardcore of if Alive

--- SniperCrosshair.lua
+++ SniperCrosshair.lua
@@ -4,7 +4,7 @@ local AWSniperCrosshair = gui.Checkbox( VIS_MAIN_REF, "lua_snipercrosshair", "Sn
 
 local function SniperCrosshair()
 
-	if AWSniperCrosshair:GetValue() and Alive then
+	if AWSniperCrosshair:GetValue() then
 		if entities.GetLocalPlayer() ~= nil then
 			local LocalPlayerEntity = entities.GetLocalPlayer();
 			local Alive = LocalPlayerEntity:IsAlive();
@@ -14,18 +14,22 @@ local function SniperCrosshair()
 			local Thirdperson = gui.GetValue( "vis_thirdperson_dist" );
 			local Scoperemover = gui.GetValue( "vis_scoperemover" );
 
-			if Scoperemover ~= 1 then
-				if ( ( isScoped == 1 or isScoped == 257 ) or Thirdperson > 0 ) then
-					client.SetConVar( "weapon_debug_spread_show", 0, true );
-				else
-					client.SetConVar( "weapon_debug_spread_show", 3, true );
+			if Alive then
+				if Scoperemover ~= 1 then
+					if ( isScoped == 1 or isScoped == 257 or Thirdperson > 0 ) then
+						client.SetConVar( "weapon_debug_spread_show", 0, true );
+					else
+						client.SetConVar( "weapon_debug_spread_show", 3, true );
+					end
+				elseif Scoperemover == 1 then
+					if ( Thirdperson > 0 and isScoped == 0 or isScoped == 257 ) then
+						client.SetConVar( "weapon_debug_spread_show", 0, true );
+					else
+						client.SetConVar( "weapon_debug_spread_show", 3, true );
+					end
 				end
-			elseif Scoperemover == 1 then
-				if ( Thirdperson > 0 and ( isScoped == 0 or isScoped == 257 ) ) then
-					client.SetConVar( "weapon_debug_spread_show", 0, true );
-				else
-					client.SetConVar( "weapon_debug_spread_show", 3, true );
-				end
+			else
+				client.SetConVar( "weapon_debug_spread_show", 0, true );
 			end
 		end
 	else
