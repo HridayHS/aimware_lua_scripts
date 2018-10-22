6a3b781dc851a06a1c666a7eddf77ab92db82f29
HridayHS <hridaysharma42@gmail.com>
2018-10-22 21:01:49 +0530

    Add lua for Engine Radar

---
+++ EngineRadar.lua
@@ -0,0 +1,19 @@
+local VIS_MAIN_REF = gui.Reference( "VISUALS", "Main" );
+
+local EngineRadarCheckbox = gui.Checkbox( VIS_MAIN_REF, "lua_engineradar", "Engine Radar", 0 );
+
+local function EngineRadar()
+
+	for index, Player in pairs( entities.FindByClass( "CCSPlayer" ) ) do
+        Player:SetProp( "m_bSpotted", EngineRadar );
+    end
+	
+	if EngineRadarCheckbox:GetValue() then
+		EngineRadar = 1
+	else
+		EngineRadar = 0
+	end
+	
+end
+
+callbacks.Register( "Draw", "Engine Radar", EngineRadar )
