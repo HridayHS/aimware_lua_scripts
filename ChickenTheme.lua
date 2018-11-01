42a7a9e53e71884a87ce63c843300baec3c10e1f
HridayHS <hridaysharma42@gmail.com>
2018-11-02 03:30:24 +0530

    ChickenTheme: Clean the code and VISUALS Master Switch Check

--- ChickenTheme.lua
+++ ChickenTheme.lua
@@ -5,21 +5,20 @@ local ChickenSkin_Combobox = gui.Combobox( VIS_OTHER_OPTIONS_REF, "lua_chickensk
 
 local ChickenScale_Slider = gui.Slider( VIS_OTHER_OPTIONS_REF, "lua_chickenscale", "Chicken Scale", 1.0, 1.0, 5.0 );
 
-local function ExtraChickenOptions()
+local function ChickensTheme()
+	if gui.GetValue( "esp_active" ) then
 
 	local Chickens = entities.FindByClass( "CChicken" );
 	local ChickenScale = ChickenScale_Slider:GetValue();
-	
+
 	for i=1, #Chickens do
-	
-		local Chicken = Chickens[i]  
-		
+		local Chicken = Chickens[i]
+
 		Chicken:SetProp( "m_nSkin", ChickenSkin );
 		Chicken:SetProp( "m_nBody", ChickenTheme );
 		Chicken:SetProp( "m_flModelScale", ChickenScale );
-
 	end
-	
+
 	-- Chicken Theme
 	if ChickenTheme_Combobox:GetValue() == 0 then
 		ChickenTheme = 0
@@ -28,23 +27,23 @@ local function ExtraChickenOptions()
 	elseif ChickenTheme_Combobox:GetValue() == 2 then
 		ChickenTheme = 3
 	elseif ChickenTheme_Combobox:GetValue() == 3 then
-		ChickenTheme = 4 
+		ChickenTheme = 4
 	elseif ChickenTheme_Combobox:GetValue() == 4 then
 		ChickenTheme = 5
 	elseif ChickenTheme_Combobox:GetValue() == 5 then
 		ChickenTheme = 1
 	end
 	-- End Chicken Theme
-	
+
 	-- Chicken Skin
 	if ChickenSkin_Combobox:GetValue() == 0 then
 		ChickenSkin = 0
-	end
-	if ChickenSkin_Combobox:GetValue() == 1 then
+	elseif ChickenSkin_Combobox:GetValue() == 1 then
 		ChickenSkin = 1
 	end
 	-- End Chicken Skin
-	
+
+	end
 end
 
-callbacks.Register( "Draw", "Chicken Theme", ExtraChickenOptions )
+callbacks.Register( "Draw", "Chickens Theme", ChickensTheme )
