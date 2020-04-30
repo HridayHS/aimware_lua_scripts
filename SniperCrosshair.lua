f275be6654de50ad4d25b230510672f03f5808aa
HridayHS <hridaysharma42@gmail.com>
2020-05-01 04:14:40 +0530

    SniperCrosshair: Update for V5

--- SniperCrosshair.lua
+++ SniperCrosshair.lua
@@ -1,41 +1,29 @@
-local VIS_MAIN_REF = gui.Reference( "VISUALS", "Shared" );
+--[[
+	Checkbox in Visuals -> Other -> Extra
+]]
 
-local SniperCrosshairCheckbox = gui.Checkbox( VIS_MAIN_REF, "lua_snipercrosshair", "Sniper Crosshair", 0 );
+local SniperCrosshair = gui.Checkbox(gui.Reference('Visuals', 'Other', 'Extra'), 'snipercrosshair', 'Sniper Crosshair', 0)
+SniperCrosshair:SetDescription('Display in-game crosshair for snipers.')
 
-local function SniperCrosshair()
-
-	if SniperCrosshairCheckbox:GetValue() then
-		if entities.GetLocalPlayer() ~= nil then
-			local LocalPlayerEntity = entities.GetLocalPlayer();
-
-			local Alive = LocalPlayerEntity:IsAlive();			
-			local isScoped = LocalPlayerEntity:GetPropInt( "m_bIsScoped" );
-	
-			local Thirdperson = gui.GetValue( "vis_thirdperson_dist" );
-			local Scoperemover = gui.GetValue( "vis_scoperemover" );
-
-			if Alive then
-				if Scoperemover ~= 1 then
-					if ( isScoped == 1 or isScoped == 257 or Thirdperson > 0 ) then
-						client.SetConVar( "weapon_debug_spread_show", 0, true );
-					else
-						client.SetConVar( "weapon_debug_spread_show", 3, true );
-					end
-				elseif Scoperemover == 1 then
-					if ( Thirdperson > 0 and isScoped == 0 or isScoped == 257 ) then
-						client.SetConVar( "weapon_debug_spread_show", 0, true );
-					else
-						client.SetConVar( "weapon_debug_spread_show", 3, true );
-					end
-				end
-			else
-				client.SetConVar( "weapon_debug_spread_show", 0, true );
-			end
+client.AllowListener('item_equip')
+callbacks.Register('FireGameEvent', function(Event)
+	if not gui.GetValue('esp.master') or not SniperCrosshair:GetValue() or Event:GetName() ~= 'item_equip' then
+		if not SniperCrosshair:GetValue() and client.GetConVar('weapon_debug_spread_show') == '3' then
+			client.SetConVar('weapon_debug_spread_show', 0, true)
+			return
 		end
-	else
-		client.SetConVar( "weapon_debug_spread_show", 0, true );
+		return
 	end
 
-end
+	local LocalPlayerIndex = client.GetLocalPlayerIndex()
+	local PlayerIndex = client.GetPlayerIndexByUserID( Event:GetInt('userid') )
+	local WeaponType = Event:GetInt('weptype')
 
-callbacks.Register( "Draw", "Sniper Crosshair", SniperCrosshair )
+	if LocalPlayerIndex == PlayerIndex then
+		if WeaponType == 5 then
+			client.SetConVar('weapon_debug_spread_show', 3, true)
+		else
+			client.SetConVar('weapon_debug_spread_show', 0, true)
+		end	
+	end
+end)
